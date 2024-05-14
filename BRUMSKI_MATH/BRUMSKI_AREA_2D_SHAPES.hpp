513d5e2d700fea4a48d8c8964804dc0e7f355342
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-05-14 19:22:13 +0100

    Update BRUMSKI_AREA_2D_SHAPES.hpp

--- BRUMSKI_MATH/BRUMSKI_AREA_2D_SHAPES.hpp
+++ BRUMSKI_MATH/BRUMSKI_AREA_2D_SHAPES.hpp
@@ -1,5 +1,5 @@
 //.This product is open to the public for use, no payment required, just giving of credits is required.
-//Made by David Tamaratare Oghenebrume, a Computer Engineering student of Rivers State University. First published on the 13th of May, 2024.
+//Made by David Tamaratare Oghenebrume, a Computer Engineering student of Rivers State University. First published on the 14th of May, 2024.
 //Last edited on 14th of May, 2024.
 
 
