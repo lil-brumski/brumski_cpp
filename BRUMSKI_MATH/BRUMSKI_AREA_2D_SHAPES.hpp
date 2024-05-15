c18588a77af790740007570604e20d6a2144c2fa
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-05-15 10:17:18 +0100

    Update BRUMSKI_AREA_2D_SHAPES.hpp

--- BRUMSKI_MATH/BRUMSKI_AREA_2D_SHAPES.hpp
+++ BRUMSKI_MATH/BRUMSKI_AREA_2D_SHAPES.hpp
@@ -1,4 +1,5 @@
 //.This product is open to the public for use, no payment required, just giving of credits is required.
+//Made by David Tamaratare Oghenebrume, a Computer Engineering student of Rivers State University. First published on the 14th of May, 2024.
 //Made by David Tamaratare Oghenebrume, a Computer Engineering student of Rivers State University. First published on the 13th of May, 2024.
 //Last edited on 14th of May, 2024.
 
@@ -6,11 +7,26 @@
 #ifndef BRUMSKI_GRADE_CALC
 #define BRUMSKI_GRADE_CALC
 
+#ifndef AREA_OF_2D_SHAPES
+#define AREA_OF_2D_SHAPES
+
+#include <iostream>
+#include <cmath>
 #include <string>
 #include <iostream>
 #include <limits>
 
 namespace BRUMSKI {
+
+    namespace AREA_2D_SHAPES {
+
+        template <class T>
+        T Input(const std::string& prompt){
+            T value;
+            while(true){
+                std::cout << prompt;
+                if(std::cin >> value){
+                    break;
 
      template <class T>
           T Input(const std::string& prompt){
@@ -36,9 +52,39 @@ namespace BRUMSKI {
             if(score >= 70 && score <= 100){
                 return 'A';
                 }
+                else{
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    std::cerr << "Wrong data type, try again!\n";
             else if(score >= 60 && score <= 69){
                 return 'B';
                 }
+            }
+            return value;
+        }
+
+        double SQUARE(double length) {
+            return std::pow(length, 2);
+        }
+
+        double RECTANGLE(double length, double width) {
+            return length * width;
+        }
+
+        double TRIANGLE(double base, double height) {
+            return 0.5 * base * height;
+        }
+
+        double CIRCLE(double radius) {
+            return M_PI * std::pow(radius, 2);
+        }
+
+        double ELLIPSE(double major_axis, double minor_axis) {
+            return M_PI * major_axis * minor_axis;
+        }
+
+        double PARALLELOGRAM(double base, double height){
+            return base * height;
             else if(score >= 50 && score <= 59){
                 return 'C';
                 }
@@ -57,5 +103,20 @@ namespace BRUMSKI {
 
         }
 
-    }
-#endif
+       double TRAPEZOID(double base1, double base2, double height){
+           return 0.5 * (base1 + base2) * height;
+       }
+
+      double RHOMBUS(double diagonal1, double diagonal2){
+          return 0.5 * diagonal1 * diagonal2;
+      }
+
+     double KITE(double diagonal1, double diagonal2){
+         return 0.5 * diagonal1 * diagonal2;
+     }
+
+    } // namespace AREA_2D
+
+} // namespace BRUMSKI
+
+#endif // AREA_OF_2D_SHAPES
