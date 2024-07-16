cbff0354961542fbe33e154a0f985831e49d8013
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-07-16 20:38:55 +0100

    Update add_sub_etc.hpp

--- brumski/math/add_sub_etc.hpp
+++ brumski/math/add_sub_etc.hpp
@@ -4,8 +4,7 @@ Knowledge of the <expected> C++ library is also needed.
 Made by David Tamaratare Oghenebrume on the 2nd of June, 2024.
 */
 
-#ifndef ADD_SUB_ETC
-#define ADD_SUB_ETC
+#pragma once
 
 #include <iostream>
 #include <cmath>
@@ -34,24 +33,24 @@ namespace brumski{
      }
      
      //Addition
-    double ADD(double a, double b){
+    double add(double a, double b){
         return a + b;
     }
     
     //Subtraction 
-    double SUB(double a, double b){
+    double sub(double a, double b){
         return a - b;
     }
      
     //Multiplication  
-    double MULTt(double a, double b){
+    double mult(double a, double b){
         return a * b;
     } 
    
    //Division 
-    std::expected<double, std::string> DIV(double a, double b){
+    std::expected<double, std::string> div(double a, double b){
         if(b == 0){
-            return std::unexpected<std::string>{"Can\'t divide by zero!"};
+            return std::unexpected<std::string>{"Can\'t divide by zero!\n"};
             }
         else{
             return a / b;
@@ -59,33 +58,41 @@ namespace brumski{
     } 
     
     //Exponents 
-    double EXP(double a, double b){
+    double exp(double a, double b){
         return std::pow(a, b);
     }  
     
     //Sine
-    double SIN(double a){
+    double sine(double a){
         std::cout<<std::fixed<<std::setprecision(2);
         return (a * (M_PI / 180));
     }
     
     //Cosine    
-    double COS(double a){
+    double cos(double a){
         std::cout<<std::fixed<<std::setprecision(2);
         return (a * (M_PI / 180));
     }
     
     //Tangent    
-    double TAN(double a){
+    double tan(double a){
         std::cout<<std::fixed<<std::setprecision(2);
         return (a * (M_PI / 180));
     }
     
     //Logarithm 
-    double LOG(double l, double b){
+    std::expected<double, std::string> log(double l, double b){
+        
+        if(l == 0){
+            return std::unexpected<std::string>{"Log cannot be zero!\n"};
+        }
+        else if(b == 0){
+            return std::unexpected<std::string>{"Base of log cannot be zero!\n"};
+        }
+        
         std::cout<<std::fixed<<std::setprecision(4);
         return std::log(l) / std::log(b);
     }
-        
+    
 }
-#endif
+
